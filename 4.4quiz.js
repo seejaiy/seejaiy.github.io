a99aaf763021238bcabcceb0b932fc6eb7f44fd5
CJ Garza <86317708+seejaiy@users.noreply.github.com>
2021-11-29 12:30:49 -0600

    Add files via upload

---
+++ 4.4quiz.js
@@ -0,0 +1,24 @@
+let number1 = 2;
+let number2 = 5;
+let number3 = 8;
+let average = (2+5+8)/3;
+console.log("The average is " + average + ".");
+function ansr() {
+    alert("The average is " + average + ".");
+}
+
+let base = 12;
+let height = 4;
+let area = 0.5*(base*height);
+console.log("The triangle's area is " + area + ".");
+function anser() {
+    alert("The triangle's area is " + area + ".");
+}
+
+let mass = 6;
+let velocity = 2;
+let ktcnrg = (mass*(velocity**2))/2;
+console.log("The kinetic energy is " + ktcnrg + ".");
+function answer() {
+    alert("The kinetic energy is " + ktcnrg + ".");
+}
