12cdefd95be0aafa3d8ac4179f1447b2f9e95c56
CJ Garza <86317708+seejaiy@users.noreply.github.com>
2021-12-06 13:06:27 -0600

    Create if.js

---
+++ u5conditionals/Lesson 2/if.js
@@ -0,0 +1,5 @@
+let age = 20;
+let quote = '"mentally stable"';
+if (age >= 18) {
+    alert("You are old enough to vote, so register now! (assuming you are a U.S. citizen, have a clean record, and fit the government's standard of " + quote + ")");
+}
